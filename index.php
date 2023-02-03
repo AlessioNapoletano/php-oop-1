d6f88316ed9f99decc2bca3ee41461af2a159f42
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-02-03 15:08:20 +0100

    Create Class Movie

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<!--
+     è definita una classe ‘Movie’
+=> all'interno della classe sono dichiarate delle variabili d'istanza
+=> all'interno della classe è definito un costruttore
+=> all'interno della classe è definito almeno un metodo
+vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
+Bonus 1:
+Modificare la classe Movie in modo che accetti piú di un genere.
+Bonus 2:
+Creare un layout completo per stampare a schermo una lista di movies.
+ -->
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
